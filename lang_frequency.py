802a409b3474eededfe2f98f5bbf07f77ac188cf
Anton1400 <akkulov.anton@gmail.com>
2016-09-28 02:35:08 +0400

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,24 @@
+#-*- coding: utf-8 -*-
+import sys
+from collections import Counter
+import re
+
+def load_data(filename):
+    with open(filename, 'r') as f:
+        text = f.read()
+    return text
 
 
-def load_data(filepath):
-    pass
-
-
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(text, count):
+    regular = re.compile("([^:.\-()+/«12–34567890»@#$%\];~№%*\s[=“?!&\,<>\^|…—]+)")
+    words = regular.findall(text)
+    frequent_words = Counter(words)
+    return frequent_words.most_common(count)
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        print('Нет параметров для запуска')
+        sys.exit(1)
+    for word, frequent in get_most_frequent_words(load_data(sys.argv[1]), 10):
+        print(word, frequent)
