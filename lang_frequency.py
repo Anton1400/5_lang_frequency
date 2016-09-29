bcfbb3dde905ffad066f31d70c8d73a72df5074c
Anton1400 <akkulov.anton@gmail.com>
2016-09-29 21:53:30 +0400

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -21,6 +21,5 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('Вы не указали путь к файлу, который нужно проанализировать')
         sys.exit(1)
-    text = load_data(sys.argv[1])
-    for word, frequency in get_most_frequent_words(text)[:TOP_WORDS]:
+    for word, frequency in get_most_frequent_words(load_data(sys.argv[1]))[:TOP_WORDS]:
         print(word, frequency)
